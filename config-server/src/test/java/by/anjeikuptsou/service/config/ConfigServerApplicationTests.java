5ab0f35d205ed72e68ad817c1a588bfcd67cfebf
Andrey Kuptsov <andrey.kuptsov@specific-group.com>
2020-01-27 10:43:47 +0300

    Config server is added

---
+++ config-server/src/test/java/by/anjeikuptsou/service/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.anjeikuptsou.service.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
