86c25decfa7513b2becfc2a1bf4c521a7acd3ae7
aakupcov <aakupcov@tut.by>
2020-01-27 08:22:40 +0300

    Contractor service is initialized

---
+++ contractor-services/src/main/java/by/anjeikuptsou/savings/contractor/ContractorServicesApplication.java
@@ -0,0 +1,13 @@
+package by.anjeikuptsou.savings.contractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorServicesApplication.class, args);
+    }
+
+}
---
+++ contractor-services/src/test/java/by/anjeikuptsou/savings/contractor/ContractorServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.anjeikuptsou.savings.contractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
